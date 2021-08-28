3e2dcb443b82b13f2b362052e3b26c5cec62ad42
YESENIA2012 <gyesenia173@gmail.com>
2021-08-28 16:13:51 -0500

    function to sort three numbers in ascending order

---
+++ findTheMiddleNumber.js
@@ -0,0 +1,41 @@
+var lea = require("prompt-sync")();
+
+var num1 = parseInt(lea("Ingrese un número: "));
+var num2 = parseInt(lea("Ingrese un número: "));
+var num3 = parseInt(lea("Ingrese un número: "));
+var menor = 0;
+var medio = 0;
+var mayor = 0;
+
+function orden(num1, num2, num3, menor, medio, mayor) {
+  if (num1 < num2 && num1 < num3) {
+    menor = num1;
+    if (num2 < num3) {
+      medio = num2;
+      mayor = num3;
+    } else {
+      medio = num3;
+      mayor = num2;
+    }
+  } else if (num2 < num3) {
+    menor = num2;
+    if (num1 < num3) {
+      medio = num1;
+      mayor = num3;
+    }
+  } else {
+    menor = num3;
+    if (num1 < num2) {
+      medio = num1;
+      mayor = num2;
+    } else {
+      medio = num2;
+      mayor = num1;
+    }
+  }
+
+  return `${menor}, ${medio}, ${mayor}`;
+}
+ordenNumeros = orden(num1, num2, num3);
+
+console.log("El orden de los números= ", ordenNumeros);
