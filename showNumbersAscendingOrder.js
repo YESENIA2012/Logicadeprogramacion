acbfd80513c58ac3e0d8e0baea544fd387c38fff
YESENIA2012 <gyesenia173@gmail.com>
2021-08-30 10:41:45 -0500

    The code was refactored to order the numbers in ascending order

--- showNumbersAscendingOrder.js
+++ showNumbersAscendingOrder.js
@@ -3,69 +3,115 @@ var lea = require("prompt-sync")();
 var num1 = parseInt(lea("Ingrese un número: "));
 var num2 = parseInt(lea("Ingrese un número: "));
 var num3 = parseInt(lea("Ingrese un número: "));
-var menor = 0;
-var medio = 0;
-var mayor = 0;
 
-function orden(num1, num2, num3, menor, medio, mayor) {
-  if (num1 < num2 && num1 < num3) {
-    menor = num1;
-    if (num2 < num3) {
-      medio = num2;
-      mayor = num3;
-    } else {
-      medio = num3;
-      mayor = num2;
-    }
-  } else if (num2 < num3) {
-    menor = num2;
-    if (num1 < num3) {
-      medio = num1;
-      mayor = num3;
-    }
-  } else {
-    menor = num3;
-    if (num1 < num2) {
-      medio = num1;
-      mayor = num2;
-    } else {
-      medio = num2;
+function findAscendenOrderWhenTwoNumbersAreEqual(num1, num2, num3) {
+  let menor = null;
+  let medio = null;
+  let mayor = null;
+
+  // num1 = 10, num2 = 10, num3 = 2
+  // num1 = 10, num2 = 10, num3 = 20
+  if (num1 == num2) {
+    if (num3 < num2) {
+      menor = num3;
       mayor = num1;
+      medio = num2;
+    } else {
+      menor = num1;
+      medio = num2;
+      mayor = num3;
     }
-    if (num1 == num2 && num1 != num3) {
-      if (num1 < num3) {
-        menor = num1;
-        medio = num2;
-        mayor = num3;
-      } else if (num3 < num1) {
-        menor = num3;
-        medio = num2;
-        mayor = num1;
-      }
-    } else if (num2 == num3 && num2 != num1) {
-      if (num2 < num1) {
-        menor = num2;
-        medio = num3;
-        mayor = num1;
-      } else if (num1 < num2) {
-        menor = num1;
-        medio = num3;
-        mayor = num2;
-      }
-    } else if (num3 == num1 && num3 != num2) {
-      if (num3 < num2) {
-        menor = num3;
-        medio = num1;
-        mayor = num2;
-      } else if (num2 < num3) {
-        menor = num2;
-        medio = num1;
-        mayor = num3;
-      }
-    }
+    return `${menor}, ${medio}, ${mayor}`;
   }
-  return `${menor}, ${medio}, ${mayor}`;
+
+  // num1 = 2, num2 = 10, num3= 10
+  // num1 = 20, num2 = 10, num3= 10
+  if (num2 == num3) {
+    if (num1 < num2) {
+      menor = num1;
+      medio = num2;
+      mayor = num3;
+    } else {
+      mayor = num1;
+      medio = num2;
+      menor = num3;
+    }
+    return `${menor}, ${medio}, ${mayor}`;
+  }
+
+  // num1 = 10, num2 = 2, num3 = 10
+  // num1 = 10, num2 = 20, num3 = 10
+  if (num1 == num3) {
+    if (num2 < num1) {
+      menor = num2;
+      medio = num1;
+      mayor = num3;
+    } else {
+      mayor = num2;
+      medio = num1;
+      menor = num3;
+    }
+    return `${menor}, ${medio}, ${mayor}`;
+  }
+
+  return null;
 }
-ordenNumeros = orden(num1, num2, num3);
+
+function findNumbersInAscendingOrder(num1, num2, num3) {
+  let menor = null;
+  let medio = null;
+  let mayor = null;
+
+  if (num1 != num2 && num1 != num3) {
+    // greater number is num1
+    // num1 = 80, num2= 10, num3= 5
+    // num1 = 80, num2= 5, num3 = 10
+
+    if (num1 > num2 && num1 > num3) {
+      mayor = num1;
+
+      if (num2 > num3) {
+        medio = num2;
+        menor = num3;
+      } else {
+        menor = num2;
+        medio = num3;
+      }
+    }
+    // greater number is num2
+    // num1 = 10, num2 = 80, num3 = 5
+    // num1= 5, num2= 80, num3 = 10
+    else if (num2 > num1 && num2 > num3) {
+      mayor = num2;
+      if (num1 > num3) {
+        medio = num1;
+        menor = num3;
+      } else {
+        menor = num1;
+        medio = num3;
+      }
+    }
+    // latest number is the greater one
+    // num1 = 10, num2 =5, num3= 80
+    // num1 = 5, num2 = 10, num3 = 80
+    else if (num3 > num1 && num3 > num2) {
+      mayor = num3;
+      if (num1 > num2) {
+        medio = num1;
+        menor = num2;
+      } else {
+        menor = num1;
+        medio = num2;
+      }
+    }
+
+    return `${menor}, ${medio}, ${mayor}`;
+  } else {
+    resultado = findAscendenOrderWhenTwoNumbersAreEqual(num1, num2, num3);
+    return resultado;
+  }
+}
+
+ordenNumeros = findNumbersInAscendingOrder(num1, num2, num3);
 
 console.log("El orden de los números= ", ordenNumeros);
