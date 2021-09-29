8c082e5f7b77a58e570481ff6e67c51ae31c6586
YESENIA2012 <gyesenia173@gmail.com>
2021-09-29 12:21:55 -0500

    changed the test function message

--- test/convertNumbersIntoZeros.test.js
+++ test/convertNumbersIntoZeros.test.js
@@ -2,8 +2,8 @@ var expect = require("chai").expect;
 //Los dos puntos y el / es para que el se salga del directorio
 const mainfunction = require("../changeVectorElementZero");
 
-describe("Should return the elements equal to the position one in zero", function () {
-  it("Should return an array with four zeros", function () {
+describe("Convert Numbers Into Zeros", function () {
+  it("SShould return the elements equal to the position one in zeros and a length of nine", function () {
     const vector = [1, 1, 1, 3, 4, 1, 5, 6, 1];
     const result = mainfunction(vector);
     const expectedResult = [1, 0, 0, 3, 4, 0, 5, 6, 0];
@@ -18,7 +18,7 @@ describe("Should return the elements equal to the position one in zero", functio
     expect(result).to.eql(expectedResult);
     expect(result).to.have.lengthOf(0);
   });
-  it("Should return the elements equal to the position one in zero and a length of seven", function () {
+  it("Should return the elements equal to the position one in zeros and a length of seven", function () {
     const vector = [2, 3, 4, 2, 6, 100, 2];
     const result = mainfunction(vector);
     const expectedResult = [2, 3, 4, 0, 6, 100, 0];
