1916193e316aefcfaf3a4bb40e7a86a5eb160b15
YESENIA2012 <gyesenia173@gmail.com>
2021-10-04 18:25:50 -0500

    The algorithm that adds the even elements of a vector was refactored

--- addEvenElementsVector.js
+++ addEvenElementsVector.js
@@ -10,13 +10,11 @@ var cantidadElementos = parseInt(
 for (contador = 0; contador < cantidadElementos; contador++) {
   let numero = parseInt(lea("Introduzca el elemento para el vector: "));
   vector[contador] = numero;
-}
-
-for (contador = 0; contador < vector.length; contador++) {
   var elementoVector = vector[contador];
 
   if (elementoVector % 2 === 0) {
     sumaElementos = sumaElementos + elementoVector;
   }
 }
+
 console.log("La suma de los elementos pares = ", sumaElementos);
