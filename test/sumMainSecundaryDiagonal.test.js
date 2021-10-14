def9e0281d51f29a16387cb9390f181d79691c0c
YESENIA2012 <gyesenia173@gmail.com>
2021-10-13 20:27:21 -0500

    The changes were made again, sexy thing

--- test/sumMainSecundaryDiagonal.test.js
+++ test/sumMainSecundaryDiagonal.test.js
@@ -12,16 +12,18 @@ describe("Add sumatory of primary and secondary diagonals", function () {
     ];
 
     const result = mainfunction(matrix);
+
     const result1 = result.sumatoriaDiagonalPrincipal;
     const result2 = result.sumatoriaDiagonalSecundaria;
 
     const expectedResult = 10;
     const expectedResult2 = 10;
+
     expect(result1).to.eql(expectedResult);
     expect(result2).to.eql(expectedResult2);
   });
 
-  it("it should show 0 as a result", function () {
+  it("should show the sum between the main and secondary diagonal", function () {
     const matrix = [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
@@ -30,11 +32,13 @@ describe("Add sumatory of primary and secondary diagonals", function () {
     ];
 
     const result = mainfunction(matrix);
+
     const result1 = result.sumatoriaDiagonalPrincipal;
     const result2 = result.sumatoriaDiagonalSecundaria;
 
     const expectedResult = 0;
     const expectedResult2 = 0;
+
     expect(result1).to.eql(expectedResult);
     expect(result2).to.eql(expectedResult2);
   });
