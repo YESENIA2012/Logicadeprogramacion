2f741147cab8a2dae26d007867d9191726951d49
YESENIA2012 <gyesenia173@gmail.com>
2021-10-13 19:15:36 -0500

    The requested changes were made

--- sumMainSecundaryDiagonal.js
+++ sumMainSecundaryDiagonal.js
@@ -1,37 +1,16 @@
-/*let lea = require("prompt-sync")();
-
-var matrix = [
-  [, , , ,],
-  [, , , ,],
-  [, , , ,],
-  [, , , ,],
-];
-
-for (contador = 0; contador < matrix.length; contador++) {
-  let filaMatrix = matrix[contador];
-  let contador2 = 0;
-  console.log("Fila: ", contador);
-  while (contador2 < filaMatrix.length) {
-    let numeroInsertarColumnas = parseInt(lea("Elemento a insertar: "));
-    filaMatrix[contador2] = numeroInsertarColumnas;
-    contador2 = contador2 + 1;
-  }
-}
-console.log(matrix);
-*/
 function sumDiagonals(matrix) {
-  var sumaDiagonalPrincipal = 0;
-  var sumaDiagonalSecundaria = 0;
+  let sumPrimaryDiagonal = 0;
+  let sumSecundaryDiagonal = 0;
 
   for (contador = 0; contador < matrix.length; contador++) {
     let filaMatrix = matrix[contador];
     for (contador2 = 0; contador2 < filaMatrix.length; contador2++) {
       let columnaMatrix = filaMatrix[contador2];
       if (contador == contador2) {
-        sumaDiagonalPrincipal = sumaDiagonalPrincipal + columnaMatrix;
+        sumPrimaryDiagonal = sumPrimaryDiagonal + columnaMatrix;
       }
       if (contador + contador2 == matrix.length - 1) {
-        sumaDiagonalSecundaria = sumaDiagonalSecundaria + columnaMatrix;
+        sumSecundaryDiagonal = sumSecundaryDiagonal + columnaMatrix;
       }
     }
   }
@@ -40,9 +19,4 @@ function sumDiagonals(matrix) {
     sumatoriaDiagonalSecundaria: sumaDiagonalSecundaria,
   };
 }
-//var diagonales = sumDiagonals(matrix);
-
-//console.log(diagonales);
-//console.log("Principal", diagonales.variableDeSumatoria);
-//console.log("Secundaria", diagonales.variableDeSumatoriaDiagonalSecundaria);
 module.exports = sumDiagonals;
