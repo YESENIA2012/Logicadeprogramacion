135d0df6404e9bf8f477e3d106275e967bfefef0
YESENIA2012 <gyesenia173@gmail.com>
2021-10-13 20:27:36 -0500

    The changes were made again, sexy thing 2

--- sumMainSecundaryDiagonal.js
+++ sumMainSecundaryDiagonal.js
@@ -1,16 +1,16 @@
 function sumDiagonals(matrix) {
-  let sumPrimaryDiagonal = 0;
-  let sumSecundaryDiagonal = 0;
+  let sumaDiagonalPrincipal = 0;
+  let sumaDiagonalSecundaria = 0;
 
   for (contador = 0; contador < matrix.length; contador++) {
     let filaMatrix = matrix[contador];
     for (contador2 = 0; contador2 < filaMatrix.length; contador2++) {
       let columnaMatrix = filaMatrix[contador2];
       if (contador == contador2) {
-        sumPrimaryDiagonal = sumPrimaryDiagonal + columnaMatrix;
+        sumaDiagonalPrincipal = sumaDiagonalPrincipal + columnaMatrix;
       }
       if (contador + contador2 == matrix.length - 1) {
-        sumSecundaryDiagonal = sumSecundaryDiagonal + columnaMatrix;
+        sumaDiagonalSecundaria = sumaDiagonalSecundaria + columnaMatrix;
       }
     }
   }
